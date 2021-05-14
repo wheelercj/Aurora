38737dcfb8b448af2f1955ea9e67a796d0d83d33
wheelercj <chrisw289@gmail.com>
2021-05-14 12:00:26 -0700

    Simplify HTML string replacement code

--- generate_site.py
+++ generate_site.py
@@ -95,11 +95,11 @@ def main(site_path, zettelkasten_path, website_title, copyright_text, hide_tags)
 
     print('Removing some unwanted CSS that was added by gh_md_to_html.')
     remove_css(all_html_paths)
-    replace_pattern(re.escape('<div class="highlight '), '<div class="', all_html_paths)
+    replace_str('<div class="highlight ', '<div class="', all_html_paths)
 
     print('Fixing some broken CSS that was added by gh_md_to_html.')
-    replace_pattern(re.escape('<div class="highlight-source-c++">'), '<div class="highlight-source-cpp">', all_html_paths)
-    replace_pattern(re.escape('<link href="/github-markdown-css/github-css.css" rel="stylesheet">'), '<link href="github-markdown-css/github-css.css" rel="stylesheet">', all_html_paths)
+    replace_str('<div class="highlight-source-c++">', '<div class="highlight-source-cpp">', all_html_paths)
+    replace_str('<link href="/github-markdown-css/github-css.css" rel="stylesheet">', '<link href="github-markdown-css/github-css.css" rel="stylesheet">', all_html_paths)
 
     print('Inserting the site header, footer, etc. into each html file.')
     append_html('index.html', f'<br><br><br><br><br><br><br><p style="text-align: center">{copyright_text}</p>')
@@ -152,6 +152,11 @@ def get_footer_html(footer=''):
     footer_html = footer_html.replace('{time_now}', time_now)
 
     return footer_html
+
+
+# Returns the total number of replacements.
+def replace_str(current, replacement, file_paths, encoding='utf8'):
+    return replace_pattern(re.escape(current), replacement, file_paths, encoding)
 
 
 # Returns the total number of replacements.
