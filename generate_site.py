7f5df9fd7bb7c9b47a3fe5880ce8770349d5193b
wheelercj <chrisw289@gmail.com>
2021-05-13 23:41:15 -0700

    Darken background of c++ code blocks

--- generate_site.py
+++ generate_site.py
@@ -95,7 +95,8 @@ def main(site_path, zettelkasten_path, website_title, copyright_text, hide_tags)
 
     print('Removing some unwanted CSS that was added by gh_md_to_html.')
     remove_css(all_html_paths)
-    n = replace_pattern(re.escape('<div class="highlight '), '<div class="', all_html_paths)
+    replace_pattern(re.escape('<div class="highlight '), '<div class="', all_html_paths)
+    replace_pattern(re.escape('<div class="highlight-source-c++">'), '<div class="highlight-source-cpp">', all_html_paths)
 
     print('Inserting the site header, footer, etc. into each html file.')
     append_html('index.html', f'<br><br><br><br><br><br><br><p style="text-align: center">{copyright_text}</p>')
