06b41d0b9dcb8a45cad95d2e48713c0b4f65bf94
wheelercj <chrisw289@gmail.com>
2021-09-16 11:50:25 -0700

    Clarify names and spacing

--- generate_my_site.py
+++ generate_my_site.py
@@ -2,8 +2,13 @@ import generate_site
 
 zettelkasten_path = 'C:/Users/chris/Documents/zettelkasten'
 site_path = 'C:/Users/chris/Documents/blog'
-website_title = "Chris' notes"
+site_title = "Chris' notes"
 copyright_text = '© 2021 Chris Wheeler'
-hide_tags = True  # If true, removes tags from the copied zettels when generating the site.
+hide_tags = True   # If true, tags will be removed from the copied 
+        ## zettels when generating the site.
 
-generate_site.main(site_path, zettelkasten_path, website_title, copyright_text, hide_tags)
+generate_site.main(site_path,
+                   zettelkasten_path,
+                   site_title,
+                   copyright_text,
+                   hide_tags)
