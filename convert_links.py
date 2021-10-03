812f9846677d0386e58e9c739ff25fcaabcfba2b
wheelercj <chrisw289@gmail.com>
2021-10-03 14:06:30 -0700

    Allow custom link ID patterns

--- convert_links.py
+++ convert_links.py
@@ -1,4 +1,5 @@
 # external imports
+import re
 from typing import List, Optional, Callable
 import logging
 
@@ -13,7 +14,8 @@ md_linker_type = Callable[[Zettel, Zettel], str]
 def convert_links_from_zk_to_md(
         zettels: List[Zettel] = [],
         zettel_paths: List[str] = [],
-        md_linker: Optional[md_linker_type] = None) -> None:
+        md_linker: Optional[md_linker_type] = None,
+        compiled_link_id_pattern: Optional[re.Pattern] = None) -> None:
     """Converts links in multiple zettels from the zk to the md format.
     
     Logs warnings for links that have valid IDs but outdated titles.
@@ -32,20 +34,30 @@ def convert_links_from_zk_to_md(
         markdown link from the first zettel to the second one. Only 
         needed for custom link formatting or if the zettels are not in 
         the same folder.
+    compiled_link_id_pattern : Optional[re.Pattern]
+        The compiled regex for the zettel ID in each zettel link. The 
+        default is `re.compile(r'(?<=\[\[)\d{14}(?=\]\])')`.
     """
     if zettel_paths:
         zettels.extend([Zettel(z) for z in zettel_paths])
     if md_linker is None:
         md_linker = lambda _, linked_z: \
             f'[{linked_z.title}]({linked_z.file_name})'
+    if compiled_link_id_pattern is None:
+        compiled_link_id_pattern = patterns.zettel_link_id
+        
     for zettel in zettels:
-        convert_zettel_links_from_zk_to_md(zettel, zettels, md_linker)
+        convert_zettel_links_from_zk_to_md(zettel,
+                                           zettels,
+                                           md_linker,
+                                           compiled_link_id_pattern)
 
 
 def convert_zettel_links_from_zk_to_md(
         zettel: Zettel,
         zettels: List[Zettel],
-        md_linker: md_linker_type) -> None:
+        md_linker: md_linker_type,
+        compiled_link_id_pattern: re.Pattern) -> None:
     """Converts links in one zettel from the zk to the md format.
     
     Logs warnings for links that have valid IDs but outdated titles.
@@ -59,11 +71,13 @@ def convert_zettel_links_from_zk_to_md(
     md_linker : Callable[[Zettel, Zettel], str]
         A function that takes two zettels as arguments and returns a 
         markdown link from the first zettel to the second one.
+    compiled_link_id_pattern : re.Pattern
+        The compiled regex for the zettel ID in each zettel link.
     """
     contents = get_contents(zettel)
     if not contents:
         return
-    link_ids: List[str] = patterns.zettel_link_id.findall(contents)
+    link_ids: List[str] = compiled_link_id_pattern.findall(contents)
     for link_id in set(link_ids):
         linked_z = get_zettel_by_id(link_id, zettels)
         if linked_z.link not in contents:
