67123a9d34e4ea494c8607334b1ea26249cee353
wheelercj <chrisw289@gmail.com>
2022-03-10 18:56:35 -0800

    Make some methods private

--- zettel.py
+++ zettel.py
@@ -11,23 +11,23 @@ class Zettel:
     def __init__(self, zettel_path: str):
         self.path: str = zettel_path
         self.folder_path: str = os.path.dirname(zettel_path)
-        self.file_name: str = self.get_zettel_file_name()  # includes extension
-        self.id: str = self.get_zettel_id()
-        self.title: str = self.get_zettel_title()
-        self.link: str = self.get_zettel_link()
-        self.tags: List[str] = self.get_zettel_tags()
+        self.file_name: str = self.__get_zettel_file_name()  # includes extension
+        self.id: str = self.__get_zettel_id()
+        self.title: str = self.__get_zettel_title()
+        self.link: str = self.__get_zettel_link()
+        self.tags: List[str] = self.__get_zettel_tags()
 
-    def get_zettel_file_name(self) -> str:
+    def __get_zettel_file_name(self) -> str:
         """Gets a zettel's file name including the extension."""
         _, name_and_extension = os.path.split(self.path)
         return name_and_extension
 
-    def get_zettel_id(self) -> str:
+    def __get_zettel_id(self) -> str:
         """Gets a zettel's ID from its path."""
         zettel_id, _ = os.path.splitext(self.file_name)
         return zettel_id
 
-    def get_zettel_title(self) -> str:
+    def __get_zettel_title(self) -> str:
         """Gets the zettel's title (its first header level 1)."""
         if self.file_name == "index.md":
             return "index"
@@ -41,14 +41,14 @@ class Zettel:
             return match[0]
         raise ValueError(f"Zettel missing a title: {self.file_name}")
 
-    def get_zettel_link(self) -> str:
-        """Gets the zettel's zettelkasten-style link
+    def __get_zettel_link(self) -> str:
+        """Gets the zettel's zettelkasten-style link.
 
         E.g. `[[20210919100142]] zettel title here`.
         """
         return f"[[{self.id}]] {self.title}"
 
-    def get_zettel_tags(self) -> List[str]:
+    def __get_zettel_tags(self) -> List[str]:
         """Gets all the tags in the zettel."""
         with open(self.path, "r", encoding="utf8") as file:
             contents = file.read()
@@ -56,7 +56,7 @@ class Zettel:
         return tags
 
     def create_html_file(self) -> str:
-        """Creates one HTML file from a markdown file in the same folder
+        """Creates one HTML file from a markdown file in the same folder.
 
         Overwrites an HTML file if it happens to have the same name.
         Returns the new HTML file's path.
