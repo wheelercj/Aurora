ae1dde5eb3a781cd6b02a15febfd63289ca605f5
wheelercj <chrisw289@gmail.com>
2022-04-08 00:10:39 -0700

    Split code for reformatting HTML files into its own file

---
+++ reformat_html.py
@@ -0,0 +1,319 @@
+# external imports
+import os
+import re
+from typing import List, Any
+from functools import cache
+from pygments import highlight, lexers  # https://pygments.org/
+from pygments.formatters import HtmlFormatter
+
+# internal imports
+from settings import Settings, settings
+from utils import logging, replace_pattern, copy_file_iff_not_present
+
+
+def reformat_html_files(
+    site_path: str, html_paths: List[str], settings: Settings
+) -> None:
+    """Reformats the HTML files.
+
+    Parameters
+    ----------
+    site_path : str
+        The path to the site's folder.
+    html_paths : List[str]
+        The absolute paths to the HTML files.
+    settings : Settings
+        The settings dictionary.
+    """
+    n = convert_attachment_links(html_paths)
+    logging.info(f"Converted {n} attachment links from the md to the html format.")
+    logging.info("Adding syntax highlighting to code in codeblocks.")
+    syntax_highlight_code(html_paths)
+
+    logging.info("Inserting the site header, footer, etc. into each file.")
+    append_copyright_notice(site_path, settings["copyright text"])
+    copy_template_html_files(site_path)
+    wrap_template_html(site_path, html_paths, settings["site title"])
+    insert_all_index_links(site_path)
+
+
+def convert_attachment_links(all_html_paths: List[str]) -> int:
+    """Converts any attachment links from the md to the html format
+
+    Returns the number of links converted.
+
+    Parameters
+    ----------
+    all_html_paths : List[str]
+        The list of all HTML files to convert links in.
+    """
+    n = replace_pattern(
+        settings["patterns"]["md link"], r'<a href="\2">\1</a>', all_html_paths
+    )
+    return n
+
+
+def syntax_highlight_code(html_paths: List[str]) -> None:
+    """Adds syntax highlighting to code inside all HTML codeblocks.
+
+    Parameters
+    ----------
+    html_paths : List[str]
+        The paths to the HTML files to add syntax highlighting to.
+    """
+    formatter = HtmlFormatter(linenos=False, cssclass="source")
+    cb_pattern = re.compile(r"<code[^<]+</code>")
+    cb_lang_pattern = re.compile(r'(?<=<code class=").+(?=">)')
+    cb_contents_pattern = re.compile(r"(?<=>)[^<]+(?=</code>)")
+
+    for html_path in html_paths:
+        with open(html_path, "r+", encoding="utf8") as file:
+            contents = file.read()
+            codeblocks: List[str] = cb_pattern.findall(contents)
+            for codeblock in codeblocks:
+                language: re.Match = cb_lang_pattern.search(codeblock)
+                lexer = None
+                if language:
+                    lexer = get_lexer(language[0])
+                if lexer:
+                    cb_contents_match = cb_contents_pattern.search(codeblock)
+                    contents = highlight_codeblock(
+                        cb_contents_match, lexer, formatter, contents
+                    )
+
+            if codeblocks:
+                file.truncate(0)
+                file.seek(0)
+                file.write(contents)
+
+
+def highlight_codeblock(
+    cb_contents_match: re.Match, lexer: Any, formatter: Any, contents: str
+) -> str:
+    """Adds syntax highlighting to the code inside an HTML codeblock
+
+    Assumes the given lexer is valid. Returns contents unchanged if
+    cb_contents_match is None.
+
+    Parameters
+    ----------
+    cb_contents_match : re.Match
+        The match object for the code inside the codeblock.
+    lexer : Any
+        The lexer to use for syntax highlighting.
+    formatter : Any
+        The formatter to use for syntax highlighting.
+    contents : str
+        The contents of the HTML file.
+    """
+    if cb_contents_match:
+        plain_codeblock = revert_html(cb_contents_match[0])
+        result = highlight(plain_codeblock, lexer, formatter)
+        contents = contents.replace(cb_contents_match[0], result, 1)
+        # Remove empty line at the end of the code block.
+        contents = contents.replace(
+            "</span>\n</pre></div>\n</code></pre>\n", "</span></pre></div></code></pre>"
+        )
+        contents = contents.replace('<div class="source"><pre>', '<div class="source">')
+        contents = contents.replace("</pre></div>", "</div>")
+    return contents
+
+
+def get_lexer(language: str) -> Any:
+    """Gets a pygments lexer by name
+
+    Returns None if a valid language is not found.
+
+    Parameters
+    ----------
+    language : str
+        The name of the language to get the lexer for.
+    """
+    if language.startswith("language-"):
+        language = language[9:]
+    if language == "cpp":
+        language == "c++"
+    return lexers.get_lexer_by_name(language)
+
+
+def revert_html(codeblock: str) -> str:
+    """Converts certain HTML strings back to plaintext
+
+    For example, `&lt;` is converted back to `<`.
+
+    Parameters
+    ----------
+    codeblock : str
+        The codeblock to revert.
+    """
+    replacements = [
+        ("&lt;", "<"),
+        ("&gt;", ">"),
+        ("&quot;", '"'),
+        ("&#39;", "'"),
+        ("&amp;", "&"),
+    ]
+    for replacement in replacements:
+        codeblock = codeblock.replace(replacement[0], replacement[1])
+    return codeblock
+
+
+def append_copyright_notice(site_path: str, copyright_text: str) -> None:
+    """Appends the site's coyright notice to the end of index.html.
+
+    Parameters
+    ----------
+    site_path : str
+        The path to the site's folder.
+    copyright_text : str
+        The copyright notice to append.
+    """
+    index_path = os.path.join(site_path, "index.html")
+    with open(index_path, "a", encoding="utf8") as file:
+        file.write(
+            '<br><br><br><br><br><br><br><p style="text-align: '
+            f'center">{copyright_text}</p>'
+        )
+
+
+def copy_template_html_files(site_path: str) -> None:
+    """Copies header and footer .html to the site folder iff they're'nt there.
+
+    Parameters
+    ----------
+    site_path : str
+        The path to the site folder.
+    """
+    copy_file_iff_not_present("header.html", site_path)
+    copy_file_iff_not_present("footer.html", site_path)
+
+
+def wrap_template_html(
+    site_path: str, all_html_paths: List[str], site_title: str
+) -> None:
+    """Wraps each HTML file's contents with a header and footer.
+
+    The header and footer used are retrieved from the copies of
+    header.html and footer.html that are in the site folder.
+
+    Parameters
+    ----------
+    site_path : str
+        The path to the site folder.
+    all_html_paths : List[str]
+        The paths to the HTML files to wrap.
+    site_title : str
+        The title of the site.
+    """
+    for path in all_html_paths:
+        with open(path, "r+", encoding="utf8") as file:
+            contents = file.read()
+            file.truncate(0)
+            file.seek(0)  # Without this, \x00 would be inserted into the front
+            # of the file.
+            if file_name_is_numeric(path):
+                header_html = get_header_html(site_title, site_path, "../")
+            else:
+                header_html = get_header_html(site_title, site_path)
+            footer_html = get_footer_html(site_path)
+            file.write(header_html + contents + footer_html)
+
+
+def file_name_is_numeric(path: str) -> bool:
+    """Determines if the file the path is for has a numeric name.
+
+    Parameters
+    ----------
+    path : str
+        The path to the file.
+    """
+    _, name_and_extension = os.path.split(path)
+    name, _ = os.path.splitext(name_and_extension)
+    return name.isnumeric()
+
+
+@cache
+def get_header_html(
+    site_title: str, site_path: str, relative_site_path: str = ""
+) -> str:
+    """Retrieves the site's header HTML from header.html.
+
+    Parameters
+    ----------
+    site_title : str
+        The title that will appear on the site.
+    site_path : str
+        The absolute path to the site's root folder.
+    relative_site_path : str
+        The relative path to the site's root folder from the HTML file
+        that this function is being called for.
+    """
+    header_file_path = os.path.join(site_path, "header.html")
+    with open(header_file_path, "r", encoding="utf8") as file:
+        header_html = file.read()
+    header_html = header_html.replace("{{site_title}}", site_title)
+    header_html = header_html.replace("{{folder}}", relative_site_path)
+    return header_html
+
+
+@cache
+def get_footer_html(site_path: str, footer_text: str = "") -> str:
+    """Retrieves the site's footer HTML from footer.html.
+
+    Parameters
+    ----------
+    site_path : str
+        The absolute path to the site's root folder.
+    footer_text : str
+        The text that will appear in the footer.
+    """
+    footer_file_path = os.path.join(site_path, "footer.html")
+    with open(footer_file_path, "r", encoding="utf8") as file:
+        footer_html = file.read()
+    footer_html = footer_html.replace("{{footer_text}}", footer_text)
+    return footer_html
+
+
+def insert_all_index_links(site_path: str) -> None:
+    """Inserts links to index files into each of the other HTML index files.
+
+    This function must not be called until after the template HTML has been
+    wrapped around each HTML file.
+
+    Parameters
+    ----------
+    site_path : str
+        The path to the site's folder.
+    """
+    index_path = os.path.join(site_path, "index.html")
+    insert_index_links(
+        index_path,
+        '<p>sort by: <a href="alphabetical-index.html">α</a> <a href="chronological-index.html">🕑</a></p>',
+    )
+    index_path = os.path.join(site_path, "alphabetical-index.html")
+    insert_index_links(
+        index_path,
+        '<p>sort by: <a href="index.html">💡</a> <a href="chronological-index.html">🕑</a></p>',
+    )
+    index_path = os.path.join(site_path, "chronological-index.html")
+    insert_index_links(
+        index_path,
+        '<p>sort by: <a href="alphabetical-index.html">α</a> <a href="index.html">💡</a>',
+    )
+
+
+def insert_index_links(file_path: str, links_to_insert: str) -> None:
+    """Inserts links to index files into one HTML index file.
+
+    Parameters
+    ----------
+    file_path : str
+        The absolute path to the HTML index file.
+    links_to_insert : str
+        The links to insert into the HTML index file.
+    """
+    with open(file_path, "r+", encoding="utf8") as file:
+        file_contents = file.read()
+        file_contents = file_contents.replace("<main>", f"<main>\n{links_to_insert}")
+        file.seek(0)
+        file.write(file_contents)
---
+++ utils.py
@@ -0,0 +1,167 @@
+# external imports
+import os
+import sys
+import re
+import shutil
+from typing import List, Tuple
+import PySimpleGUI as sg
+import logging
+
+# internal imports
+from settings import settings
+
+
+__log_path = os.path.join(os.path.dirname(__file__), "zk-ssg.log")
+logging.basicConfig(
+    filename=__log_path, encoding="utf-8", filemode="w", level=logging.INFO
+)  # https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+
+
+def copy_file_iff_not_present(file_name: str, site_path: str) -> str:
+    """Copies a file from this dir to the site dir iff it's not there.
+
+    Returns the file's path whether it was already there or newly copied.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to copy.
+    site_path : str
+        The path to the site folder.
+    """
+    site_file_path = os.path.join(site_path, file_name)
+    if os.path.isfile(site_file_path):
+        logging.info(f"  {file_name} already exists. The file will not be replaced.")
+    else:
+        logging.info(f"  {file_name} was not found. Providing a new copy.")
+        shutil.copy(file_name, site_path)
+    return site_file_path
+
+
+def get_file_contents(absolute_path: str, encoding: str) -> str:
+    """Gets a file's contents.
+
+    If UnicodeDecodeError is raised, this function will log and show an
+    error message and make the program exit.
+
+    Parameters
+    ----------
+    absolute_path : str
+        The absolute path to the file.
+    encoding : str
+        The encoding of the file.
+    """
+    with open(absolute_path, "r", encoding=encoding) as file:
+        try:
+            contents = file.read()
+        except UnicodeDecodeError as e:
+            logging.error(f"UnicodeDecodeError: {e}")
+            sg.popup(
+                "Error: one or more symbols cannot not be decoded "
+                f"as unicode in file {absolute_path}"
+            )
+            sys.exit(f"UnicodeDecodeError: {e}")
+        else:
+            return contents
+
+
+def replace_pattern(
+    compiled_pattern: str,
+    replacement: str,
+    file_paths: List[str],
+    encoding: str = "utf8",
+    file_must_exist: bool = False,
+) -> int:
+    """Replaces a regex pattern with a string in multiple files
+
+    Returns the total number of replacements.
+
+    Parameters
+    ----------
+    compiled_pattern : str
+        The regex pattern to search for.
+    replacement : str
+        The string to replace the pattern with. This can be a regex group
+        reference.
+    file_paths : List[str]
+        The paths to the files to search in.
+    encoding : str
+        The encoding of the files.
+    file_must_exist : bool
+        Whether any file paths matched by the compiled pattern must exist.
+        Assumes the compiled pattern is for searching for file paths but will
+        sometimes match other things too.
+    """
+    total_replaced = 0
+
+    for file_path in file_paths:
+        contents = get_file_contents(file_path, encoding)
+
+        # Temporarily remove any code blocks from contents.
+        triple_codeblocks = settings["patterns"]["triple codeblock"].findall(contents)
+        if len(triple_codeblocks):
+            contents = settings["patterns"]["triple codeblock"].sub("␝", contents)
+
+        single_codeblocks = settings["patterns"]["single codeblock"].findall(contents)
+        if len(single_codeblocks):
+            contents = settings["patterns"]["single codeblock"].sub("␞", contents)
+
+        # Replace the pattern.
+        if not file_must_exist:
+            contents, n_replaced = compiled_pattern.subn(replacement, contents)
+        else:
+            contents, n_replaced = replace_file_paths(
+                compiled_pattern, replacement, contents
+            )
+        total_replaced += n_replaced
+
+        # Put back the code blocks.
+        for single_codeblock in single_codeblocks:
+            contents = re.sub(
+                r"␞", single_codeblock.replace("\\", r"\\"), contents, count=1
+            )
+        for triple_codeblock in triple_codeblocks:
+            contents = re.sub(
+                r"␝", triple_codeblock[0].replace("\\", r"\\"), contents, count=1
+            )
+
+        # Save changes.
+        if n_replaced > 0:
+            with open(file_path, "w", encoding=encoding) as file:
+                file.write(contents)
+
+    return total_replaced
+
+
+def replace_file_paths(
+    path_pattern: re.Pattern, replacement: str, contents: str
+) -> Tuple[str, int]:
+    """Replaces all file paths in the contents with the correct relative path.
+
+    Parameters
+    ----------
+    path_pattern : re.Pattern
+        The compiled regex pattern to search for.
+    replacement : str
+        The string to replace the pattern with. This can be a regex group
+        reference.
+    contents : str
+        The contents to replace the file links in.
+
+    Returns
+    -------
+    str
+        The contents with the file links replaced.
+    int
+        The number of replacements made.
+    """
+    start = 0
+    n_replaced = 0
+    while True:
+        match = path_pattern.search(contents, start)
+        if not match:
+            return contents, n_replaced
+        if os.path.isfile(match[0]):
+            contents = path_pattern.sub(replacement, contents, count=1)
+            n_replaced += 1
+        start = match.end()
